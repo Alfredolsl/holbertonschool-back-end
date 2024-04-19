2323a9cd48ec31a2e7debc65ca103b037eb66f56
root <alfredo.445566@gmail.com>
2024-04-19 01:15:44 +0000

    script executes if its run as main program

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,19 +3,20 @@
 import requests
 from sys import argv
 
-link = "https://jsonplaceholder.typicode.com"
+if __name__ == "__main__":
+    link = "https://jsonplaceholder.typicode.com"
 
-employee_id = argv[1]
-employee = requests.get(f"{link}/users?id={employee_id}").json()
-employee_name = employee[0]["name"]
+    employee_id = argv[1]
+    employee = requests.get(f"{link}/users?id={employee_id}").json()
+    employee_name = employee[0]["name"]
 
-todo_list = requests.get(f"{link}/todos?userId={employee_id}").json()
-total_tasks = len(todo_list)
-tasks_done = [task for task in todo_list if task["completed"] is True]
+    todo_list = requests.get(f"{link}/todos?userId={employee_id}").json()
+    total_tasks = len(todo_list)
+    tasks_done = [task for task in todo_list if task["completed"] is True]
 
-print("Employee {} is done with tasks({}/{}):".format(employee_name,
-                                                       len(tasks_done),
-                                                       total_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+                                                           len(tasks_done),
+                                                           total_tasks))
 
-for task in tasks_done:
-    print("\t ", task["title"], sep="")
+    for task in tasks_done:
+        print("\t ", task["title"], sep="")
