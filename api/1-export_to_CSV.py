c38d1e067946f516dfc68e0534cb261979592487
root <alfredo.445566@gmail.com>
2024-04-19 01:37:20 +0000

    imports now in alphabetical order

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Export employee's data into a csv file"""
+import csv
 import requests
 from sys import argv
-import csv
 
 if __name__ == "__main__":
     link = "https://jsonplaceholder.typicode.com"
