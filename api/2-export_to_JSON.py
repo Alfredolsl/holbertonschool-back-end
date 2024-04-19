5ed86d6a4e7ee127b2cb057d07bdd390d331dbc3
root <alfredo.445566@gmail.com>
2024-04-19 01:55:37 +0000

    exports employee's data to a json file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Exports employee's data into a json file"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    link = "https://jsonplaceholder.typicode.com"
+
+    employee_id = argv[1]
+    employee = requests.get(f"{link}/users/{employee_id}").json()
+    employee_name = employee.get("username")
+
+    todo_list = requests.get(f"{link}/todos?userId={employee_id}").json()
+
+    tasks = {employee_id: [{"task":todo.get("title"),
+             "completed": todo.get("completed"),
+             "username": employee_name} for todo in todo_list]}
+
+    file_name = f"{employee_id}.json"
+    with open(file_name, mode="w", encoding="utf-8") as f:
+        json.dump(tasks, f)
