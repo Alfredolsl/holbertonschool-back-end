91ebc96296e2642a1c679d6e5ee36a14cb433496
root <alfredo.445566@gmail.com>
2024-04-19 01:04:43 +0000

    pycodestyle compliant

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,15 +3,19 @@
 import requests
 from sys import argv
 
+link = "https://jsonplaceholder.typicode.com"
+
 employee_id = argv[1]
-employee = requests.get(f"https://jsonplaceholder.typicode.com/users?id={employee_id}").json()
+employee = requests.get(f"{link}/users?id={employee_id}").json()
 employee_name = employee[0]["name"]
 
-todo_list = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}").json()
+todo_list = requests.get(f"{link}/todos?userId={employee_id}").json()
 total_tasks = len(todo_list)
 tasks_done = [task for task in todo_list if task["completed"] is True]
 
-print("Employee {} is done with tasks ({}/{}):".format(employee_name, len(tasks_done), total_tasks))
+print("Employee {} is done with tasks ({}/{}):".format(employee_name,
+                                                       len(tasks_done),
+                                                       total_tasks))
 
 for task in tasks_done:
     print("\t", task["title"], sep="")
